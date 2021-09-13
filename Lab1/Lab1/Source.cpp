2f5f17c745972896bf3c0343e679412909989024
Sasha Videvich <fpm.videvich@bsu.by>
2021-09-13 19:53:47 +0300

    fixed Lab1

--- Lab1/Lab1/Source.cpp
+++ Lab1/Lab1/Source.cpp
@@ -9,13 +9,13 @@ int main() {
 	cin >> k >> x;
 	while (true) {
 		xn = pow(x, n) / (-n);
-		answer2 = answer2 + xn;
-		n = n + 1;
+		answer2 += xn; // answer2 = answer2 + xn;
+		n += 1;
 		if (abs(xn) <= 1 / pow(10, k)) {
 			break;
 		}
 	}
-	answer1 = log10(1 - x);
+	answer1 = log(1 - x);
 	cout << answer1 << '\n';
 	cout << answer2 << endl;
 	cout << answer1 - answer2;
